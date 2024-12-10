3844905a0c5782a6944d845e2940dc123fa3cffc
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-10 19:35:56 -0300

    Move estrutura do Livro para o componente

--- src/components/Livro.jsx
+++ src/components/Livro.jsx
@@ -3,7 +3,13 @@ import "./Livro.css";
 function Livro() {
   return (
     <div className="livro-container">
-      LIVRO FUNCIONA
+      <img src="" alt="capa do livro 1" />
+          <div>
+            <h3>livro 1</h3>
+            <p>Autor: xxxxxxxxxxxx</p>
+            <p>Ano de publicação: XXXX</p>
+            <p>Classificação: ★★★★★</p>
+          </div>
     </div>
   );
 }
