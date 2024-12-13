ae41fc6a994075999d0a1f25b6896d0a7d19a7f4
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-12-12 22:26:04 -0300

    Estiliza botão

--- src/components/Livro.jsx
+++ src/components/Livro.jsx
@@ -6,10 +6,11 @@ function Livro({capa, titulo, autor, ano, classificacao, link}) {
       <img src={capa} alt={`Capa do livro ${titulo}`} />
           <div className="info-container">
             <h3>{titulo}</h3>
+            <hr />
             <p>Autor: {autor}</p>
             <p>Ano de publicação: {ano}</p>
             <p>Classificação: {"★".repeat(classificacao)}</p>
-            <p><a href={link}>Compre aqui!</a></p>
+            <a href={link} target="blank" className="livro-link">Compre aqui!</a>
           </div>
     </div>
   );
